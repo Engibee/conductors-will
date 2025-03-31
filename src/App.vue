4a22fbe5966d85170393cd25058abb336288c847
Vinicius Louzada <viniciuslouzada99@hotmail.com>
2025-03-31 15:45:41 -0300

    - Organized the UI to be most simplistic. - Added a file in utils to help formatting texts. - Finished the basic logic: Demand, Purchasables, Automation and Ticks.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <script setup>
 import { usePaperclipGame } from './composables/usePaperclipGame.js'
-const { paperclips, fioDisponivel, FabricarPaperclip } = usePaperclipGame()
+import { formatPrice } from './utils/helpers/formatPrice.js';
+const { copperWireinMeter, funds, priceOfCopper, availableCopper, workers, week, month, year, FabricarPaperclip, buy_worker, buy_refined_copper, buy_chance, increasePrice, decreasePrice } = usePaperclipGame()
 </script>
 
 <template>
-  <div>
-    <button @click="FabricarPaperclip">Make paperclip</button>
-    <h3>Paperclip amount: {{ paperclips }}</h3>
-    <h3>Fio disponível: {{ fioDisponivel }}m</h3>
+  <div class="time">
+  📅 Week: {{ week }} | 📆 Month: {{ month }} | 📅 Year: {{ year }}
   </div>
+    <div class="card business">
+      <h3>Copper wire: {{ copperWireinMeter }} meters</h3>
+      <button @click="FabricarPaperclip">Make Copper Wire</button>
+      
+      <h5>Business</h5>
+      <p>Funds: ${{ formatPrice(funds) }}</p>
+      <p class="sellprice">
+        <button @click="decreasePrice" class="small-btn">-</button>
+        <button @click="increasePrice" class="small-btn">+</button>
+        Selling price: ${{ formatPrice(priceOfCopper) }}
+      </p>
+      <p>Demanda: <strong>{{ formatPrice(buy_chance(priceOfCopper)) }}</strong></p>
+      <p>Refined Copper: {{ formatPrice(availableCopper) }} kg</p>
+      <button @click="buy_refined_copper">Buy Refined Copper</button>
+      <p class="mini_info">Price: $9.795</p>
+    </div>
+    <div class="card automation">
+      <h3>Automation</h3>
+      <div class="autoinfo">
+        <button @click="buy_worker">Buy worker</button>
+        <p>Price: NaN</p>
+        <p>Amount of workers: {{ workers }}</p>
+      </div>
+      <p class="mini_info">Generates 1 meter of copper per tick(second)</p>
+    </div>
 </template>
 
 <script>
--- src/composables/usePaperclipGame.js
+++ src/composables/usePaperclipGame.js
@@ -1,23 +1,127 @@
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
+
 
 export function usePaperclipGame(){
 
-const paperclips = ref(0)
-const fioDisponivel = ref(100)
-const QtdDeFioPorClipe = 0.036
+onMounted(() => {
+    intervalo = setInterval(update_tick, 1000)
+    console.log(`O contador foi montado.`)
+    })
+onUnmounted(() => {
+    // Limpa o intervalo quando o componente for destruído
+    clearInterval(intervalo)
+    console.log(`O contador foi limpado/destruído.`)
+    })
+const ticks = ref(0)
+let intervalo = null
+const copperWireinMeter = ref(0)
+const availableCopper = ref(10)
+const copperQtPerMeter = 0.02232
+const funds = ref(30)
+const priceOfCopper = ref(0.50)
+const kgOfCopper = 9.795
+const workers = ref(0)
+const workersPrice = ref(150.0)
+const week = ref(0)
+const month = ref(0)
+const year = ref(0)
+
+const update_tick = () => {
+    ticks.value++
+    // Ação a cada 1 tick
+  if (ticks_events(5)) {
+    buy_simulation_per_tick()
+  }
+  if (ticks_events(10)){
+    time_handler()
+  }
+  if (ticks_events(1)){
+    automation_handler()
+  }
+}
+function automation_handler(){
+  if (availableCopper.value >= (copperQtPerMeter * workers.value)){
+  copperWireinMeter.value += 1 * workers.value
+  availableCopper.value -= (1 * workers.value) * copperQtPerMeter
+  }
+  else{
+    //Nothing yet.
+  }
+}
+function buy_refined_copper(){
+  if (funds.value >= kgOfCopper){
+    availableCopper.value += 1.0
+    funds.value -= kgOfCopper
+  }
+}
+function buy_worker(){
+  if (funds.value >= workersPrice.value){
+    funds.value -= workersPrice.value
+    workers.value++
+  }
+}
+function time_handler(){
+  if (month.value == 12 && week.value == 4){
+    year.value++
+    month.value = 0
+    week.value = 0
+  }
+  else if (week.value == 4){
+    month.value++
+    week.value = 0
+  }
+  else{
+    week.value++
+  }
+}
+function ticks_events(tick){
+  return ticks.value % tick === 0
+}
+
+function buy_chance(price){
+  return 0.8 * Math.exp(-((price - 0.50) / 0.20))
+}
+
+function buy_simulation_per_tick(){
+  const chance = buy_chance(priceOfCopper.value);
+  if (Math.random() < chance) {
+    const quantity = Math.floor(Math.random() * 10) + 1;
+    if (copperWireinMeter.value >= quantity){
+    copperWireinMeter.value -= quantity;
+    funds.value += quantity * priceOfCopper.value
+    }
+  }
+}
 
 function FabricarPaperclip(){
-    console.log('Antes de fabricar: ', paperclips.value, fioDisponivel.value)
-    if (fioDisponivel.value >= QtdDeFioPorClipe){
-        fioDisponivel.value -= QtdDeFioPorClipe
-        paperclips.value++
+    if (availableCopper.value >= copperQtPerMeter){
+        availableCopper.value -= copperQtPerMeter
+        copperWireinMeter.value++
     }
-    console.log('Depois de fabricar: ', paperclips.value, fioDisponivel.value)
+}
+function increasePrice(){
+    priceOfCopper.value += 0.10
+}
+function decreasePrice(){
+  if (priceOfCopper.value > 1e-6){
+    priceOfCopper.value -= 0.10
+  }
 }
 return{
-    paperclips,
-    fioDisponivel,
-    FabricarPaperclip
+    copperWireinMeter,
+    availableCopper,
+    funds,
+    priceOfCopper,
+    workers,
+    week,
+    month,
+    year,
+    FabricarPaperclip,
+    buy_worker,
+    buy_refined_copper,
+    buy_chance,
+    increasePrice,
+    decreasePrice
 }
 
 }
